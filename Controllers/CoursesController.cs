7d0e39a766d46ace9f2636c7f068effc789bd7c1
marcotrunc <marcotrunc@gmail.com>
2022-07-25 12:33:54 +0200

    Added the structure of the Courses Index Page

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCourse.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(string id)
+        {
+            return Content($"Sono Detail, ho ricevuto l'{id}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,13 +15,27 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+//to config the routing
 app.UseRouting();
 
 app.UseAuthorization();
 
+//to create a single Route (default route), puù essere sostituito da app.MapDefaultControllerRoute
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+/* old version of Asp.Net
+ * but first of this we need to add in Startup.cs =>
+ *  public void ConfigureServices(IserviceCollection services)
+ *  {
+ *  services.AddMvc();
+ *  }
+app.UseMvc(routeBuilder =>
+{
+                                        /courses  /detail  /5
+    routeBuilder.MapRoute("default", "{controller}/{action}/ {id?}");
+});
+*/
 
 app.Run();
